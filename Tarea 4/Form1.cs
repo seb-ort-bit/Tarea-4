ae6374045557324944512da9754aa3170fe715b6
seb-ort-bit <seb.ort.backup@gmail.com>
2023-03-23 18:22:24 -0400

    Parte gráfica del formulario Login

---
+++ Tarea 4/Form1.cs
@@ -0,0 +1,15 @@
+namespace Tarea_4
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
